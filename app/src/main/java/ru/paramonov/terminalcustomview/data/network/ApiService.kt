667dcade0ec11e917aa15b7def84882f5e2ff9a3
nikita <paramonov20@bk.ru>
2024-01-02 06:40:30 +0300

    Реализовал масштабирование в приложении для графиков, с помошью transformableState.

--- app/src/main/java/ru/paramonov/terminalcustomview/data/network/ApiService.kt
+++ app/src/main/java/ru/paramonov/terminalcustomview/data/network/ApiService.kt
@@ -5,6 +5,6 @@ import ru.paramonov.terminalcustomview.data.model.Result
 
 interface ApiService {
 
-    @GET("aggs/ticker/AAPL/range/30/minute/2023-01-01/2024-01-01?adjusted=true&sort=asc&limit=50000&apiKey=fRrpu3FjJixOINdzFcgmIKJCNZWfFaPr")
+    @GET("aggs/ticker/AAPL/range/30/minute/2023-01-01/2024-01-01?adjusted=true&sort=desc&limit=50000&apiKey=fRrpu3FjJixOINdzFcgmIKJCNZWfFaPr")
     suspend fun loadBars(): Result
 }
--- app/src/main/java/ru/paramonov/terminalcustomview/presentation/component/Terminal.kt
+++ app/src/main/java/ru/paramonov/terminalcustomview/presentation/component/Terminal.kt
@@ -2,38 +2,54 @@ package ru.paramonov.terminalcustomview.presentation.component
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.TransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import ru.paramonov.terminalcustomview.data.model.Bar
+import kotlin.math.roundToInt
+
+private const val MIN_VISIBLE_BARS_COUNT = 20
 
 @Composable
 fun Terminal(
     bars: List<Bar>
 ) {
+    var visibleBarsCount by remember {
+        mutableStateOf(value = 100)
+    }
+
+    val transformableState = TransformableState { zoomChange, _, _ ->
+        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+            .coerceIn(minimumValue = MIN_VISIBLE_BARS_COUNT, maximumValue = bars.size)
+    }
+
     Canvas(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color.Black)
+            .transformable(state = transformableState)
     ) {
-        // ширина свечи
-        val barWidth = size.width / bars.size
-        // максимальная стоимость за всё время
         val maxPriceOnTime = bars.maxOf { bar -> bar.highPrice }
-        // минимальная стоимость за всё время
         val minPriceOnTime = bars.minOf { bar -> bar.lowPrice }
-        // кол-во пискселей на один доллар
-        val pxPerPoint = size.height / (maxPriceOnTime - minPriceOnTime)
 
-        bars.forEachIndexed { index, bar ->
-            val offsetX = index * barWidth
+        val barWidth = size.width / visibleBarsCount
+        val pxPerCount = size.height / (maxPriceOnTime - minPriceOnTime)
+
+        bars.take(n = visibleBarsCount).forEachIndexed { index, bar ->
+            val offsetX = size.width - index * barWidth
             drawLine(
                 color = Color.White,
-                start = Offset(x = offsetX, y = size.height - (bar.lowPrice - minPriceOnTime) * pxPerPoint),
-                end = Offset(x = offsetX, y = size.height - (bar.highPrice - minPriceOnTime) * pxPerPoint),
+                start = Offset(x = offsetX, y = size.height - ((bar.lowPrice - minPriceOnTime) * pxPerCount)),
+                end = Offset(x = offsetX, y = size.height - ((bar.highPrice - minPriceOnTime) * pxPerCount)),
                 strokeWidth = 1.dp.toPx()
             )
         }
