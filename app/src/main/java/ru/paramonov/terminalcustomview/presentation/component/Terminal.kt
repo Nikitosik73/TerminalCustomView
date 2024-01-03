c4cd18594b586c96aae88267dbfff5f3130305fb
nikita <paramonov20@bk.ru>
2024-01-03 23:20:58 +0300

    Попрактиковался с ListSaver, написал свой TerminalSaver для хранения состояния экрана с графиком

--- app/src/main/java/ru/paramonov/terminalcustomview/presentation/component/Terminal.kt
+++ app/src/main/java/ru/paramonov/terminalcustomview/presentation/component/Terminal.kt
@@ -6,15 +6,13 @@ import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.unit.dp
 import ru.paramonov.terminalcustomview.data.model.Bar
 import kotlin.math.roundToInt
@@ -25,38 +23,19 @@ private const val MIN_VISIBLE_BARS_COUNT = 20
 fun Terminal(
     bars: List<Bar>
 ) {
-    var visibleBarsCount by remember {
-        mutableStateOf(value = 100)
-    }
-
-    var scrolledBy by remember {
-        mutableStateOf(0f)
-    }
-
-    var terminalIWidth by remember {
-        mutableStateOf(0f)
-    }
-
-    val barWidth by remember {
-        derivedStateOf {
-            terminalIWidth / visibleBarsCount
-        }
-    }
-
-    val visibleBars by remember {
-        derivedStateOf {
-            val startIndex = (scrolledBy / barWidth).roundToInt().coerceAtLeast(minimumValue = 0)
-            val endIndex = (startIndex + visibleBarsCount).coerceAtMost(maximumValue = bars.size)
-            bars.subList(fromIndex = startIndex, toIndex = endIndex)
-        }
-    }
+    var terminalState by rememberTerminalState(bars = bars)
 
     val transformableState = TransformableState { zoomChange, panChange, _ ->
-        visibleBarsCount = (visibleBarsCount / zoomChange).roundToInt()
+        val visibleBarsCount = (terminalState.visibleBarsCount / zoomChange).roundToInt()
             .coerceIn(minimumValue = MIN_VISIBLE_BARS_COUNT, maximumValue = bars.size)
 
-        scrolledBy = (scrolledBy + panChange.x)
-            .coerceIn(minimumValue = 0f, maximumValue = bars.size * barWidth - terminalIWidth)
+        val scrolledBy = (terminalState.scrolledBy + panChange.x)
+            .coerceIn(minimumValue = 0f, maximumValue = bars.size * terminalState.barWidth - terminalState.terminalWidth)
+
+        terminalState = terminalState.copy(
+            visibleBarsCount = visibleBarsCount,
+            scrolledBy = scrolledBy
+        )
     }
 
     Canvas(
@@ -64,15 +43,17 @@ fun Terminal(
             .fillMaxSize()
             .background(color = Color.Black)
             .transformable(state = transformableState)
+            .onSizeChanged { size ->
+                terminalState = terminalState.copy(terminalWidth = size.width.toFloat())
+            }
     ) {
-        terminalIWidth = size.width
-        val maxPriceOnTime = visibleBars.maxOf { bar -> bar.highPrice }
-        val minPriceOnTime = visibleBars.minOf { bar -> bar.lowPrice }
+        val maxPriceOnTime = terminalState.visibleBars.maxOf { bar -> bar.highPrice }
+        val minPriceOnTime = terminalState.visibleBars.minOf { bar -> bar.lowPrice }
 
         val pxPerCount = size.height / (maxPriceOnTime - minPriceOnTime)
-        translate(left = scrolledBy) {
+        translate(left = terminalState.scrolledBy) {
             bars.forEachIndexed { index, bar ->
-                val offsetX = size.width - index * barWidth
+                val offsetX = size.width - index * terminalState.barWidth
                 drawLine(
                     color = Color.White,
                     start = Offset(x = offsetX,y = size.height - ((bar.lowPrice - minPriceOnTime) * pxPerCount)),
@@ -84,7 +65,7 @@ fun Terminal(
                     color = if (isIncreasePrice) Color.Green else Color.Red,
                     start = Offset(x = offsetX, y = size.height - ((bar.openPrice - minPriceOnTime) * pxPerCount)),
                     end = Offset(x = offsetX, y = size.height - ((bar.closePrice - minPriceOnTime) * pxPerCount)),
-                    strokeWidth = barWidth / 2
+                    strokeWidth = terminalState.barWidth / 2
                 )
             }
         }
